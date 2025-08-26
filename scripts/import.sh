60f562c161ade6b2773056e7226d616c29982618
Marko Baric <marko.baric@pasteur.fr>
2025-08-26 11:43:15 +0200

    fix(docker config):Add comments and modify import.

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -c
 sleep 2
-ls /data/b/neo4j-admin-import-call.sh
-if [ -f /data/b/neo4j-admin-import-call.sh ]; then
-  chmod +x /data/b/neo4j-admin-import-call.sh
-  /data/b/neo4j-admin-import-call.sh
+ls /data/jobim/neo4j-admin-import-call.sh
+if [ -f /data/jobim/neo4j-admin-import-call.sh ]; then
+  chmod +x /data/jobim/neo4j-admin-import-call.sh
+  /data/jobim/neo4j-admin-import-call.sh
 fi
 neo4j start
 sleep 10
